e673af2f323916086d279a90bcd054fec120f59a
Sara Hendrix <seh18@email.sc.edu>
2024-11-05 11:35:57 -0500

    deployment attempt 1

--- newpage/src/index.js
+++ newpage/src/index.js
@@ -11,7 +11,7 @@ import MyBooks from "./pages/mybooks";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
